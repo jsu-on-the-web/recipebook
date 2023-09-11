a46e83e59458f148ab4859edd324dff9893d3bf5
Janet Suryadi <janet@suryadi.id.au>
2023-09-11 23:05:31 +1000

    Improving the appearance of the cards a little

--- lib/main.dart
+++ lib/main.dart
@@ -54,10 +54,22 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget buildRecipeCard(Recipe recipe) {
     return Card(
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       child: Column(
         children: <Widget>[
-          Image(image: AssetImage(recipe.imgUrl)),
-          Text(recipe.label),
+          Image(image: AssetImage(recipe.imgUrl), width: 250.0),
+          const SizedBox(
+            height: 12.0,
+          ),
+          Text(
+            recipe.label,
+            style: const TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Roboto Sans',
+            ),
+          ),
         ],
       ),
     );
